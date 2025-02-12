874ebd3d470a618551aa8a5243357c4fe76cc669
Joel Cutler <joelcutler108@gmail.com>
2025-02-12 17:04:26 +0000

    added anonymous authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
 import { AnimatePresence } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { AlertProvider } from "./components/Alert";
 import { cleanupGames } from "./firebase/db";
+import { firebaseSignIn } from "./firebase/firebase";
 import ErrorPage from "./pages/ErrorPage";
 import GamePage from "./pages/GamePage";
 import MenuPage from "./pages/MenuPage";
 
 export default function App() {
+	const [firebaseAuthed, setFirebaseAuthed] = useState<boolean>(false);
+
 	useEffect(() => {
-		cleanupGames();
+		firebaseSignIn().then((u) => {
+			setFirebaseAuthed(true);
+			console.log(u);
+			cleanupGames();
+		});
 	}, []);
 
 	const location = useLocation();
 
+	// todo display something whilst we are authenticating
+
 	return (
 		<AlertProvider>
 			<AnimatePresence mode="wait">
-				<Routes key={location.pathname} location={location}>
-					<Route path="/" element={<MenuPage />} />
-					<Route path="/game/:code" element={<GamePage />} />
-					<Route path="*" element={<ErrorPage error="Error 404" description="Page not found" />} />
-				</Routes>
+				{firebaseAuthed && (
+					<Routes key={location.pathname} location={location}>
+						<Route path="/" element={<MenuPage />} />
+						<Route path="/game/:code" element={<GamePage />} />
+						<Route path="*" element={<ErrorPage error="Error 404" description="Page not found" />} />
+					</Routes>
+				)}
 			</AnimatePresence>
 		</AlertProvider>
 	);
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,12 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, signInAnonymously } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import firebaseConfig from "./firebaseConfig";
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const db = getDatabase(firebaseApp);
+export const auth = getAuth();
+
+export async function firebaseSignIn() {
+	return signInAnonymously(auth);
+}
